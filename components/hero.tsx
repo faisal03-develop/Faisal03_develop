0c4211edffab646cf09bc50177ca741066cc1da2
Faisal Altaf <altaffaisal532@gmail.com>
2025-11-27 12:57:55 +0500

    Added the 3D Laptop

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,18 @@
+import Image from "next/image";
+import Laptop from "./laptop";
 
 export default function hero() {
   return (
-      <div className="max-w-[1024px] max-h-[423px] pt-[65px] font-(family-name:--font-firaCode) flex flex-row items-center">
+      <div className="max-w-[1024px] h-[423px] pt-[65px] font-(family-name:--font-firaCode) flex flex-row items-center justify-between">
         <div className="max-w-[537px] max-h-[227px] flex flex-col ">
           <p className="font-semibold text-3xl">Faisal is a <span className="text-(--primary)">Web Developer</span> expert in <span className="text-(--primary)">NextJs</span> & <span className="text-(--primary)">MERN Stack</span></p>
           <p className="text-(--gray) text-base mt-8">He crafts responsive websites where technologies meet creativity</p>
           <button className="border bborder-solid border-violet-500 w-[148px] h-[37px] mt-6">Contact Me !!</button>
         </div>
-        <div>
+        <div className="max-h-[469px] max-w-[386px]">
+          <Image src="/icons/OutlineLogo.svg" alt="Logo" height={155} width={155} className="z-0"/>
+          <Laptop />
+          {/* <Image src="/icons/Dots.svg" alt="Logo" height={84} width={84}/> */}
 
         </div>
     </div>
---
+++ components/laptop.tsx
@@ -0,0 +1,13 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <div className='h-full w-full'>
+        <Spline
+          scene="https://prod.spline.design/L6XfCSS4v3tQyLTs/scene.splinecode" 
+        />
+      </div>
+    </main>
+  );
+}
