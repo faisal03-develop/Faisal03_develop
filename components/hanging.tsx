f1d88f4b794ae563e48218709563d0b69e1cbc81
Faisal Altaf <altaffaisal532@gmail.com>
2025-11-27 12:12:11 +0500

    Fiexed the Hanging Image

--- components/hanging.tsx
+++ components/hanging.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default function hanging() {
   return (
-    <div className="w-[32px] h-[311px] absolute top-0 left-15 flex flex-col items-center">
-        <Image src="/icons/line10.svg" alt="Logo" width={2} height={191} />
+    <div className="w-[32px] h-[250px] absolute top-0 left-[60px] flex flex-col items-center">
+        <Image src="/icons/line10.svg" alt="Logo" width={1} height={191} />
         <div className="mt-[8px] flex flex-col gap-[8px]">
             <Link href="#">
             <Image src="/icons/Github.svg" alt="Github" height={32} width={32}/>
