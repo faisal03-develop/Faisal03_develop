3fe9459160ea22aaa9aeb15dbc5a1320d89aa1fb
Faisal Altaf <altaffaisal532@gmail.com>
2025-11-27 14:47:09 +0500

    Fixed Laptop Dimensions

--- components/laptop.tsx
+++ components/laptop.tsx
@@ -3,10 +3,10 @@ import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <main>
-      <div className='h-full w-full'>
-        <Spline
-          scene="https://prod.spline.design/L6XfCSS4v3tQyLTs/scene.splinecode" 
-        />
+      <div className='h-80 w-80 z-20'>
+      <Spline
+        scene="https://prod.spline.design/9j7znkyfu825p0VH/scene.splinecode"
+      />
       </div>
     </main>
   );
