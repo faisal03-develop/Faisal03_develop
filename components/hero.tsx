26992640a5a52a4c119923039c927cfc3ced5e46
Faisal Altaf <altaffaisal532@gmail.com>
2025-11-27 12:01:23 +0500

    Added the Hero Element

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,10 +1,11 @@
 
-
 export default function hero() {
   return (
     <div className="max-w-[1024px] max-h-[423px] pt-[65px] font-(family-name:--font-firaCode)">
       <div>
-        <p className="font-semibold text-3xl max-w-[537px]">Faisal is <span className="text-(--primary)">Web Developer</span> expert in <span className="text-(--primary)">NextJs</span> & <span className="text-(--primary)">MERN Stack</span></p>
+        <p className="font-semibold text-3xl max-w-[537px]">Faisal is a <span className="text-(--primary)">Web Developer</span> expert in <span className="text-(--primary)">NextJs</span> & <span className="text-(--primary)">MERN Stack</span></p>
+        <p className="text-(--gray) text-base mt-[32px]">He crafts responsive websites where technologies meet creativity</p>
+        <button className="border-solid border-violet-700">Contact Me !!!</button>
       </div>
         
     </div>
