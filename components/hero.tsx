fd719faa4b9611a1a1c6536ad5b9464cbeaafab5
Faisal Altaf <altaffaisal532@gmail.com>
2025-11-27 12:21:13 +0500

    Completed the ContentArea of the Hero Section

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,15 @@
 
 export default function hero() {
   return (
-    <div className="max-w-[1024px] max-h-[423px] pt-[65px] font-(family-name:--font-firaCode)">
-      <div>
-        <p className="font-semibold text-3xl max-w-[537px]">Faisal is a <span className="text-(--primary)">Web Developer</span> expert in <span className="text-(--primary)">NextJs</span> & <span className="text-(--primary)">MERN Stack</span></p>
-        <p className="text-(--gray) text-base mt-[32px]">He crafts responsive websites where technologies meet creativity</p>
-        <button className="border-solid border-violet-700">Contact Me !!!</button>
-      </div>
-        
+      <div className="max-w-[1024px] max-h-[423px] pt-[65px] font-(family-name:--font-firaCode) flex flex-row items-center">
+        <div className="max-w-[537px] max-h-[227px] flex flex-col ">
+          <p className="font-semibold text-3xl">Faisal is a <span className="text-(--primary)">Web Developer</span> expert in <span className="text-(--primary)">NextJs</span> & <span className="text-(--primary)">MERN Stack</span></p>
+          <p className="text-(--gray) text-base mt-8">He crafts responsive websites where technologies meet creativity</p>
+          <button className="border bborder-solid border-violet-500 w-[148px] h-[37px] mt-6">Contact Me !!</button>
+        </div>
+        <div>
+
+        </div>
     </div>
   )
 }
