7e3531dcab30feea80e868fc9d7c6e70d168dabe
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-01 14:36:31 +0500

    Finallized the 4D Element

--- components/spidy.tsx
+++ components/spidy.tsx
@@ -3,13 +3,11 @@ import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <main>
-        <div className='h-96'>
-
+        <div className='h-[400px]'>
       <Spline
-        scene="https://prod.spline.design/6WRqzk0v0xKIY8xz/scene.splinecode" 
+        scene="https://prod.spline.design/pUb4fJmWeFhOTqwO/scene.splinecode" 
       />
         </div>
-
     </main>
   );
 }
