e0ee97677447c16c71524510dc4934fc361d4812
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-01 11:59:36 +0500

    Completed the Skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image"
 import Logolog from "./logolog"
 
+
 export default function skills() {
   return (
-    <div>
-        <div className="flex flex-row font-(family-name:--font-firaCode) justify-center items-center text max-w-[701px] max-h-10 text-3xl">
+    <div className="font-(family-name:--font-firaCode)">
+        <div className="flex flex-row  justify-center items-center text max-w-[701px] max-h-10 text-3xl">
             <p><span className="text-primary)">#</span>Skills</p>
             <Image src="/icons/line7.svg" alt="line" width={711} height={1} className="ml-4"/>
+        </div>
+        <div>
+          <div className="border-2 border-solid border-(--gray) p-4 mt-6">
+            <p className="mb-3 font-bold">Technical Skills: </p>
+            <p className="text-(--gray) text-sm">HTML, CSS, javaScript, Next, React, NodeJs, ExpressJs, PostGreSQL, PHP, C++...</p>
+          </div>
+          
+          <div className="border-2 border-solid border-(--gray) p-4 my-4">
+            <p className="mb-3 font-bold">Tools: </p>
+            <p className="text-(--gray) text-sm">Visual Studio, VS Code, MySQL Server, MONGODB, Git, Github, POSTMAN, Stripe, Clerk...</p>
+          </div>
         </div>
         <div className="py-8">
             <Logolog />
