2fe99e56bdba64baa3e5e8d36863b8ec4f125434
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-12 17:52:32 +0500

    Changed the 3d Element to Robot.

--- components/spidy.tsx
+++ components/spidy.tsx
@@ -1,3 +1,48 @@
+// import Spline from '@splinetool/react-spline/next';
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <div className='h-[90%] w-[90%] md:h-[500px] md:w-[650px]'>
+//       <Spline
+//         scene="https://prod.spline.design/nD1PzlwPGO37154V/scene.splinecode" 
+//       />
+//       </div>
+//     </main>
+//   );
+// }
+
+//Abstract
+
+// import Spline from '@splinetool/react-spline/next';
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <div className='h-[90%] w-[90%] md:h-[500px] md:w-[650px]'>
+//       <Spline
+//         scene="https://prod.spline.design/p78VBolg54pLgsCf/scene.splinecode" 
+//       />
+//       </div>
+//     </main>
+//   );
+// }
+
+// import Spline from '@splinetool/react-spline/next';
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <div className='h-[90%] w-[90%] md:h-[500px] md:w-[650px]'>
+//       <Spline
+//         scene="https://prod.spline.design/aNRyMNnRFGzwT2mQ/scene.splinecode" 
+//       />
+// </div>
+//     </main>
+//   );
+// }
+
+
 import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
@@ -5,7 +50,7 @@ export default function Home() {
     <main>
       <div className='h-[90%] w-[90%] md:h-[500px] md:w-[650px]'>
       <Spline
-        scene="https://prod.spline.design/nD1PzlwPGO37154V/scene.splinecode" 
+        scene="https://prod.spline.design/p0NXbpnWeAgQ8hX1/scene.splinecode" 
       />
       </div>
     </main>
