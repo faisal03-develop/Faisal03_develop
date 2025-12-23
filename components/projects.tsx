a8ed742fc3b69bfa9419f22619982fce53645da6
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-23 12:15:46 +0500

    Added Portfolio Website and link

--- components/projects.tsx
+++ components/projects.tsx
@@ -60,9 +60,9 @@ export default function projects() {
 
             {/* Project 6 */}
             <ProjectCard 
-                href=""
-                title="Ghost Cursor"
-                image="/images/ghost.png"
+                href="https://faisal03-develop.github.io/sample/"
+                title="Portfolio Website"
+                image="/images/portfolio.jpg"
                 stack={["NextJs", "ReactJs", "TailwinsCSS", "TypeScript"]}
             />
         </div>
